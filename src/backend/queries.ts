082028139ae47ec5b4cf0dd695d0e1e24c9fe06d
NexxonOfficial <nexxonofficial@gmail.com>
2024-09-10 20:22:48 +0100

    push: thread creation checking in queries

--- src/backend/queries.ts
+++ src/backend/queries.ts
@@ -7,13 +7,18 @@ mysql.config({
     host     : process.env.DB_ENDPOINT,
     database : process.env.DB_DATABASE,
     user     : process.env.DB_USERNAME,
-    password : process.env.DB_PASSWORD
+    //password : process.env.DB_PASSWORD
 })
 
 export const EnableIfDisabled = async (guildId: Number) => {
     return new Promise(async (res, rej) => {
         let response = await mysql.query(`SELECT * FROM ${process.env.DATABASE_THREADS} WHERE guild_id = ?`, [guildId])
-        
-        console.log(response);
     })
+}
+
+export const ValidateThreadExistence = async (guild_id: Number) => {
+    let threadCheck = await mysql.query(`SELECT * FROM ${process.env.DATABASE_THREADS} WHERE guild_id = ?`, [guild_id])
+    if(threadCheck == '') {
+        await mysql.query(`INSERT INTO ${process.env.DATABASE_THREADS} (guild_id) VALUES (?)`, [guild_id])
+    }
 }
--- src/events/guildCreate.ts
+++ src/events/guildCreate.ts
@@ -1,9 +1,10 @@
 import { ArgsOf, Discord, On } from "discordx";
+import { ValidateThreadExistence } from "../backend/queries.ts";
 
 @Discord()
 class GuildCreate {
     @On({event: "guildCreate"})
     async guildCreate(guildInfo: ArgsOf<"guildCreate">) {
-
+        ValidateThreadExistence(parseInt(guildInfo[0].id as string))
     }
 }
